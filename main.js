18ed72efabd8a37273a474059005276001af06a3
Rachel Kelly <racheldk126@gmail.com>
2022-06-01 09:58:59 -0700

    dates formatted and names capitalized - final

--- main.js
+++ main.js
@@ -18,13 +18,11 @@ for (let customer of customers) {
     imageElement.classList.add("br-100", "h3", "w3", "dib")
     cardElement.appendChild(imageElement)
 
-    //add name
+    //add name, capitalize first letter of first and last names
     let nameElement = document.createElement('h1')
     nameElement.classList.add('f4')
-    nameElement.innerText = `${customer.name.first} ${customer.name.last}`
+    nameElement.innerText = `${customer.name.first.charAt(0).toUpperCase(customer.name.first) + customer.name.first.slice(1)} ${customer.name.last.charAt(0).toUpperCase(customer.name.last) + customer.name.last.slice(1)}`
     cardElement.appendChild(nameElement)
-
-    //try to put cute line border thing here?
 
     //email
     let emailElement = document.createElement('div')
@@ -46,49 +44,19 @@ for (let customer of customers) {
    console.log(addressTwoElement.innerText)
    cardElement.appendChild(addressTwoElement)
    
-
    //DOB
-   let birthdateElement = document.createElement('div')
+   let birthdateElement = document.createElement('p')
    birthdateElement.classList.add("lh-copy", "measure", "center", "f6", "black-70")
-//    let adjustedDate = DateTime(customer.dob.date)
-//     console.log(adjustedDate)
-// DateTime.now(customer.dob.date).toObject()
-// console.log(DateTime.fromISO(`customer.dob.date`).toFormat('yyyy LLL dd'))
-
-   birthdateElement.innerText = `DOB: ${customer.dob.date}`
+    dobFormat = moment(customer.dob.date).format("MMM Do, YYYY")
+   birthdateElement.innerText = `DOB: ${dobFormat}`
    cardElement.appendChild(birthdateElement)
-    //---- get DOB to look normal
 
    //customer since 
    let registrationDateElement = document.createElement('p')
    registrationDateElement.classList.add("lh-copy", "measure", "center", "f6", "black-70")
-   registrationDateElement.innerText = `Customer since: ${customer.registered.date}`
+   registrationDateElement.innerText = `Customer since: ${moment(customer.registered.date).format("MMM Do, YYYY")}`
    cardElement.appendChild(registrationDateElement)
-   //---- get date to look normal
-
 
    //make the card show up on the page
     customerList.appendChild(cardElement)
-
 }
-
-
-// {/* <div>
-//       <article class="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
-//         <img src="" alt=""  class="br-100 h3 w3 dib"/>
-//         <h1 class="f4">first name last name</h1>
-//         <p class="lh-copy measure center f6 black-70">email</p>
-//         <hr class="mw3 bb bw1 b--black-10">
-//         <p class="lh-copy measure center f6 black-70">street number street name</p>
-//         <p class="lh-copy measure center f6 black-70">street number street name</p>
-//         <p class="lh-copy measure center f6 black-70">city state zipcode</p>
-//         <p class="lh-copy measure center f6 black-70">
-//           <p>DOB:</p>
-//           date
-//         </p>
-//         <p class="lh-copy measure center f6 black-70">
-//           <p>Customer Since:</p>
-//           registration date
-//         </p>
-//       </article>
-//     </div> */}
